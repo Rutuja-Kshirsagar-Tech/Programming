b669ad015e434eaf150bcd69166bdd47835d5141
Rutuja Kshirsagar <kshirsagarr806@gmail.com>
2026-06-12 15:54:26 +0530

    Update Program86.java

--- Java_Programming/Competitive/Assignment/Program86.java
+++ Java_Programming/Competitive/Assignment/Program86.java
@@ -14,13 +14,13 @@ class Logic
 {
     void checkLeapYear(int year)
     {
-        if((year % 4) == 0  && (year % 100) == 0)
+        if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
         {
             System.out.println("Leap year");
         }
         else
         {
             System.out.println("Not a leap year");
-        } 
+        }
     }
 }
