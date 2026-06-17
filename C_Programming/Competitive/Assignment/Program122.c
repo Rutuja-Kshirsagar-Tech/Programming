ca62e338ee879f382c8b5b8f96b177e81b57fcc7
RutujaK-hub <kshirsagarr806@gmail.com>
2026-06-18 04:03:50 +0530

    Pattern Printing

---
+++ C_Programming/Competitive/Assignment/Program122.c
@@ -0,0 +1,54 @@
+// Accept number of rows and number of columns from user and
+// display below pattern
+
+// Input :  iRow = 4    iCol = 4
+// Output : *   *   *   *    
+//          *   *   *   #   
+//          *   *   #   #
+//          *   #   #   #
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid input\nNumber of rows and columns should be same\n");
+        return;
+    }
+    
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j <= i)
+            {
+                printf("%c\t",'*');
+            }
+            else
+            {
+                printf("%c\t",'#');
+            }
+            
+        }
+        printf("\n");
+    } 
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
