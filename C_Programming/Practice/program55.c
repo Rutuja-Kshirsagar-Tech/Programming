748cd73b7f6d6aac4b05299883ee52aa588a0ffa
RutujaK-hub <kshirsagarr806@gmail.com>
2026-06-20 23:54:10 +0530

    Problems on numbers

---
+++ C_Programming/Practice/program55.c
@@ -0,0 +1,25 @@
+// Input : 5
+// Output : 1 2 3 4
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    for(int iCnt = 1; iCnt < iNo; iCnt = iCnt + 1)
+    {
+        printf("%d\t",iCnt);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
