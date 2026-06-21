45195ad1f5f6ab3bdeff6c1dbdcaffcbb81931e8
RutujaK-hub <kshirsagarr806@gmail.com>
2026-06-21 19:52:59 +0530

    Problem on Digits

---
+++ Java_Programming/Practice/program92.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class program92
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.ReverseDigits(iValue);
+
+        System.out.println("Reverse number is : "+iRet);
+    }
+}
+
+class DigitX
+{
+    public int ReverseDigits(int iNo)
+    {
+        int iDigit = 0;
+        int iRev = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            iRev = iRev * 10 + iDigit;
+            
+            iNo = iNo / 10;
+        }
+        return iRev;
+    }
+} 
+    
+
+
